6bf7432ff65db076605fad18f94a3d1d2a8fa284
paulo-ufmg <paulo-rocha@ufmg.br>
2024-05-20 01:17:45 -0300

    Feito até letra a e b,  da Lista03-Desafio

--- Lista03/Bioclass.py
+++ Lista03/Bioclass.py
@@ -16,19 +16,26 @@ class Bioprof:
      - Paulo Rocha (paulo-rocha@ufmg.br)
      - Rodrigo Jereissati Martins (rod.jereissati@gmail.com)
 
-    Link do github: https://github.com/paulo-ufmg/Bioclass
+    Link do github: https://github.com/paulo-ufmg/Bioprof
     """
     
     def __init__(self):
-       self.__version__ = "0.01"
+       self.__version__ = "0.02"
+       self.id = "" #string que contém o nome(identificação da sequencia)
        self.ids = [] #Lista de id's das sequências referenciadas pelo index da lista
        self.info ={} #Dicionario que armazena um comentario da sequência
-       self.nucleotideo = {"A":0,"C":0,"G":0,"T":0,"U":0} #nucleotideos presente na sequencia
-       self.nucleotideos = [] #Lista de aminoácido {"C":0,"G":0,"T":0,"U":0}
+       self.composicao_DNA_RNA_PROTEINA = {} #nucleotideos ou aminoácidos presente na sequencia DNA-RNA ou de Proteina
+       #self.nucleotideo = {} #nucleotideos ou aminoácidos presente na sequencia DNA-RNA ou de Proteina
+       self.composicao_total = [] #Totalização de nucleotídeos na sequencia  {"C":0,"G":0,"T":0,"U":0}
+       self.sequencia = [] # Lista de string contendo a sequencia de DNA,RNA ou Proteina
        self.alerta = True
-       self.sequencia = [] #Lista completa das sequencias
-       
-       
+       self.codons = {
+            "UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L","CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L","AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M","GUU": "V",
+            "GUC": "V", "GUA": "V", "GUG": "V","UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S","CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P","ACU": "T", "ACC": "T",
+            "ACA": "T", "ACG": "T","GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A","UAU": "Y", "UAC": "Y", "UAA": "*", "UAG": "*","CAU": "H", "CAC": "H", "CAA": "Q",
+            "CAG": "Q","AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K","GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E","UGU": "C", "UGC": "C", "UGA": "*", "UGG": "W",
+            "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R","AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R","GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"           
+        }
 
     def message_view(self,mensagem,interrupt=False):
         """Exibir mensagem de alerta"""
@@ -36,64 +43,125 @@ class Bioprof:
         if(interrupt): exit()        
         
 
-    def seq_existe (self,id):
-       """Retorna verdadeiro se a sequencia identificada por id está registrada"""
-       return True if(id in self.ids) else False 
+    def seq_existe (self,arg):
+       """Retorna verdadeiro se a sequencia identificada em argumento está registrada"""
+       if(isinstance(arg, int)): return True if 0 <= arg < len(self.ids) else False #procura por um indice presente na Lista de identificação de sequencias (ids)
+       else: return True if(arg in self.ids) else False  #procura pela identificação da sequencia na Lista de identificação de sequencias (ids)
     
     def get_seqs(self):
         """Retorna a lista de sequencias registradas"""
         return  self.ids
+    
+    def get_info(self,id_ou_indice):
+        """Retorna comentário da sequencia"""
+        indice = id_ou_indice if (isinstance(id_ou_indice, int)) else self.ids.index(id_ou_indice)
+        if indice in self.info:
+            return self.info[indice]
+        else: self.message_view("Sequencia não possui comentário!")   
         
-    def soma_nucleotideos(self,indice):
-        """Soma a quantidade nucleotideos de uma sequencia"""
-        total = 0
-        if((len(self.nucleotideos) < indice) or not isinstance(indice, int)): 
+        
+    def get_tamanho_sequencia(self,id_ou_indice): #permite receber como paramentro um inteiro que é o indice ou uma string id que é a identificação da sequencia
+        """Soma a quantidade nucleotideos ou aminoácidos de uma sequencia"""
+        indice = id_ou_indice if (isinstance(id_ou_indice, int)) else self.ids.index(id_ou_indice)
+        tamanho = 0
+        if((len(self.composicao_total) < indice) or not isinstance(indice, int)): 
             self.message_view("Sequencia não encontrada!")
             return 0
-        for n in self.nucleotideos[indice]:
-            total += self.nucleotideos[indice][n]
-        return total    
+        for n in self.composicao_total[indice]:
+            tamanho += self.composicao_total[indice][n]
+        return tamanho
 
-    def new_seq(self,id):
+    def nova_sequencia(self,id):
+       """Insere uma nova sequencia na lista ids"""
        if not(self.seq_existe(id)):
           self.ids.append(id)
-          self.nucleotideos.append(dict(self.nucleotideo)) #Expressão dict faz uma copia do dicionario nucleotideo
+          self.composicao_total.append(dict(self.composicao_DNA_RNA_PROTEINA)) #Expressão dict faz uma copia do dicionario
+          self.sequencia.append("") #Inicializa uma string na lista para aramazenar a sequencia
+          return True 
        else:
-          self.message_view("Sequencia já inserida!",True)   
+          self.message_view("Sequencia já inserida!")   
+          return False
+       
+    def get_sequencia(self,id):
+        """ Retorna a sequencia armazenada com seus nucleotídeos (DNA e RNA) ou dos aminoácidos (Proteina)"""
+        return self.sequencia[ self.ids.index(id) ]  if (self.seq_existe(id)) else None
+    
+    def get_composicao_seq(self,id):
+        """ Retorna a composição da sequência com os totais de nucleotídeos (DNA e RNA) ou dos aminoácidos (Proteina) """
+        if (self.seq_existe(id)):
+            temp = f"T({self.get_tamanho_sequencia(self.ids.index(id))}): "
+            for chave, valor in self.composicao_total[ self.ids.index(id) ].items():
+                temp +=  chave +'('+str(valor)+')'+','
+            return temp
+        self.message_view("Sequencia não encontrada!")   
+
+    def get_tipo_seq(self,id):
+        """Identifica uma sequencia se é de DNA, RNA ou PROTEINA"""
+        seq = self.get_sequencia(id)
+        if(seq.find("U") != -1):  
+            return "RNA"
+        return "DNA" if(len(re.findall(r'[^ACGT]', seq)) == 0) else "Proteina" #Se sequencia contém somente ACGT é um DNA
+    
+    def get_percentual_GC(self,arg):
+        """Retorna a percentual(%) de Guanina e Citosina na sequência."""
+        if (self.seq_existe(arg)):
+            indice = arg if isinstance(arg, int) else self.ids.index(arg) 
+            G = self.composicao_total[indice]['G'] if('G' in self.composicao_total[indice]) else 0
+            C = self.composicao_total[indice]['C'] if('C' in self.composicao_total[indice]) else 0
+            GC = ( G + C ) / self.get_tamanho_sequencia(indice) if(self.get_tamanho_sequencia(indice) > 0) else 0
+            return GC*100
+        self.message_view("Sequencia não encontrada!")
+        return None
+
+
 
     def insert_comment(self,id,info):
+       """Armazena o comentário da sequência"""
        if (self.seq_existe(id) and info is not None):
             self.info[self.ids.index(id)] = info
      
-    def add_nucleotideo(self,id,tipo):
-        """Adciona um somatório de nucleotídos da sequência"""
-        if(re.sub(r'[\t\n\r\f\v\b\0\a\s]', '',tipo)==""):
+    def add_item(self,id,item):
+        """Adciona um somatório de nucleotídos ou aminoácidos da sequência"""
+        if(re.sub(r'[\t\n\r\f\v\b\0\a\s]', '',item)==""):
             return 
         if(self.seq_existe(id)): 
-            if("ACGTU".find(tipo) != -1):
-               self.nucleotideos[ self.ids.index(id) ][tipo] += 1
+            if("ACGTUDEFHIKLMNPQRSVWY".find(item) != -1): #ACGTU (Nucleotídeos presentes no DNA ou RNA) | ACDEFGHIKLMNPQRSTVWY (Aminoácidos presentes na Proteina)
+                indice = self.ids.index(id)
+                if(item in self.composicao_total[ indice ]):
+                    self.composicao_total[ indice ][item] += 1
+                else:
+                    self.composicao_total[ indice ][item] = 1    
+                self.sequencia[ indice ] += item    
             else:
-               self.message_view(f"Nucleotídeo não reconhecido na sequencia[{id}]!{tipo}",True)
+               self.message_view(f"Um nucleotídeo ou aminoácido não reconhecido na sequencia[{id}]!{item}",True)
         else:
             self.message_view(f"-Identificação de sequência não encontrada!{id}")
                     
-    def get_info_seq(self,id):
+    def ver_info_seq(self,id):
+        """Exibe dados de uma sequência"""
         if(self.seq_existe(id)):
             indice = self.ids.index(id)
             print("Informações da sequência:")
             print("=========================")
             print(f"Id: {self.ids[indice]}")
+            print(f"Sequencia de {self.get_tipo_seq(id)}")
             if(indice in self.info ):
                 print(f"Info: {self.info[indice]}")
-            #print(self.nucleotideos)   
-            print(f"Q(n): A({self.nucleotideos[indice]['A']}),C({self.nucleotideos[indice]['C']}),G({self.nucleotideos[indice]['G']}),T({self.nucleotideos[indice]['T']}),U({self.nucleotideos[indice]['U']})")
-            print(f"Total: {self.soma_nucleotideos(indice)}")
-            GC = (self.nucleotideos[indice]['G']+self.nucleotideos[indice]['C'])/self.soma_nucleotideos(indice)
-            print("GC: {:.2f} %".format(GC*100))
+            print(f"Composição: {self.get_composicao_seq(id)}")
+                #print(f"Q(n): A({self.composicao_total[indice]['A']}),C({self.composicao_total[indice]['C']}),G({self.composicao_total[indice]['G']}),T({self.composicao_total[indice]['T']}),U({self.composicao_total[indice]['U']})")
+            print(f"Tamanho sequencia: {self.get_tamanho_sequencia(indice)}")
+            print("Percentual de GC: {:.2f} %".format(self.get_percentual_GC(indice)))
             
-            
+            #if(("G" in self.composicao_total[indice])and("C" in self.composicao_total[indice])):
+            #    GC = (self.composicao_total[indice]['G']+self.composicao_total[indice]['C'])/self.get_tamanho_sequencia(indice)
+            #    print("GC: {:.2f} %".format(GC*100))
+
+
+        else:
+            self.message_view("Sequencia não encontrada!")   
        
     def identifica_id(self,line):
+        """Retorna a string de identificação da sequência (id)"""
         match = re.match(r'^>(\S+)', line)
         if match:
             return match.group(1)
@@ -101,6 +169,7 @@ class Bioprof:
             return None
 
     def identifica_info(self,line):
+        """Retorna a string de comentário da sequência (id)"""
         match = re.match(r'^>[\w\|]+ (.+)', line)
         if match:
             return match.group(1)
@@ -109,19 +178,104 @@ class Bioprof:
         
     def leiaArquivoFasta(self,nome_arquivo):
         """ Lê  todas as sequências de um arquivo Fasta """    
-        with open (nome_arquivo) as arq:
+        with open (nome_arquivo, "r") as arq:
             while True:
                 self.line = arq.readline()
-                if not self.line:
-                    break
-                if self.line.strip()[0]==";":
-                    continue
+                if not self.line: break #interrompe ao final do arquivo
+                if self.line.strip() == "": continue  # Ignorar linhas em branco
+                if self.line.strip()[0]==";": continue # ignora linhas de comentário
+
                 if(self.line.find('>') >= 0): #Identificado nova sequencia
                     self.id = self.identifica_id(self.line)
-                    self.new_seq(self.id)
-                    comment = self.identifica_info(self.line)
-                    self.insert_comment(self.id,comment)
+                    if(self.nova_sequencia(self.id)):
+                        comment = self.identifica_info(self.line)
+                        self.insert_comment(self.id,comment)
+                    else: self.id = None    
                 else:    #compilando a sequencia
-                    for i in [*self.line]:
-                        self.add_nucleotideo(self.id,i)    
-#===============================================================
+                    if(self.id != None):
+                        for i in [*self.line]:
+                            self.add_item(self.id,i)    
+
+    def add_seq(self,n1,n2,n3):
+        """Insere uma sequência através de código"""
+        if(self.nova_sequencia(n1)):
+            self.insert_comment(n1,n2)
+            for n in n3:
+                self.add_item(n1,n) 
+        else:
+            self.message_view("Erro ao adicionar ou sequência já inserida!")   
+
+    # Calcula a distância de Hamming (dH) entre duas sequências
+    def dH(self,id1,id2):
+        """Distãncia de Hamming (dH) entre duas sequências de DNA ou RNA"""
+        if(self.seq_existe(id1) and self.seq_existe(id2)):
+            if([self.get_tipo_seq(id1),self.get_tipo_seq(id2)] == ['DNA','DNA'] or [self.get_tipo_seq(id1),self.get_tipo_seq(id2)] == ['RNA','RNA']):
+                if(self.get_tamanho_sequencia(id1)==self.get_tamanho_sequencia(id2)):
+                    seq1 = self.get_sequencia(id1)
+                    seq2 = self.get_sequencia(id2)
+                    dist =0 
+                    for n1, n2 in zip(seq1, seq2):
+                        if(n1 != n2):
+                            dist += 1
+                    return dist       
+                else: self.message_view("As sequências têm comprimentos diferentes.")       
+            else: self.message_view("Para calcular a distância de hamming as sequências devem ser de DNA ou de RNA.")       
+        else: self.message_view("Identificação das sequências não encontrada!")       
+        return False
+    
+    def rna2proteina(self,id):
+        proteina = []
+        if(self.seq_existe(id)):
+            if(self.get_tipo_seq(id) == "RNA"):
+                mRNA = self.get_sequencia(id)
+                for i in range(0, len(mRNA), 3):
+                    codon = mRNA[i:i+3]  # Extrai o códon de 3 nucleotídeos
+                    aminoacido = self.codons.get(codon, 'X')  # Obtém o aminoácido correspondente ao códon
+                    proteina.append(aminoacido)  # Adiciona o aminoácido à lista
+                # Junta os aminoácidos em uma única string
+                return ''.join(proteina) 
+            else: self.message_view("Sequência para transcrição não é um RNA!")              
+        else: self.message_view("Identificação das sequências não encontrada!")              
+        return None
+    
+    def transc_dna2rna(self,id):
+        """Transcrição de uma sequência de DNA em um mRNA -Substitui todas as ocorrências de 'T' por 'U' """
+        if(self.seq_existe(id)):
+            if(self.get_tipo_seq(id) == "DNA"):
+                DNA = self.get_sequencia(id)
+                mRNA =  DNA.replace('T', 'U')
+                return mRNA
+            else: self.message_view("Sequência para transcrição não é um DNA!")              
+        else: self.message_view("Identificação das sequências não encontrada!")              
+        return None
+
+
+    def get_kmers(self,id,k):
+        """Retorna uma lista de sequencias k da janela deslizante da sequência
+        :param sequencia: A sequência de entrada (string)
+        :param k: O comprimento dos k-mers (inteiro)
+        :return: Uma lista de k-mers
+        """
+        if(self.seq_existe(id)):
+            if(k <= self.get_tamanho_sequencia(id)):
+                sequencia = self.get_sequencia(id)
+                kmers = []
+                for i in range(len(sequencia) - k + 1):
+                    kmers.append(sequencia[i:i + k])
+                return kmers
+            else: self.message_view("Comprimento k (k-mers) maior que a sequência!")                  
+        else: self.message_view("Identificação das sequências não encontrada!")  
+        return None                
+    
+    def get_assembly_kmers(self):
+        """Assembly, monta um genoma a partir dos kmers (conjunto de todas as sequencias do objeto)"""
+        if(len(self.ids) > 1):
+            genoma = ""
+            for i in range(len(self.ids)-1):
+                genoma += self.sequencia[i][0]
+            genoma += self.sequencia[len(self.ids)-1]
+            return genoma
+        if (len(self.ids) == 1): return self.ids[0]
+        else: self.message_view("Não existe Mers suficientes para construir uma estrutura de sequência!")                  
+        return None
+    
---
+++ Lista03/desafio03.py
@@ -0,0 +1,84 @@
+from Bioclass import Bioprof
+import os
+import sys
+
+arquivo = None if(len(sys.argv) < 3) else sys.argv[2]
+arg = None if(len(sys.argv) < 3 ) else sys.argv[1]
+
+if(arquivo==None)and (len(sys.argv)!= 3):
+    print("Argumento incorreto! leitura [opção] nome_aquivo_fasta [ opcçao -i | -o ]")
+    exit()
+
+if(arg !='-i'):
+    print("Argumento incorreto! leitura [opção] nome_aquivo_fasta  [ opcçao -i | -o ]")
+    exit()
+
+seq = Bioprof()
+
+seq.leiaArquivoFasta(arquivo)
+print(seq.get_seqs())
+
+orginal =  seq.get_seqs()[0] 
+
+#Solução exercício Letra a) do desafio03.py
+#-------------------------------------------
+print("a) Exibir o tamanho da sequência e o percentual de GC")
+print("-----------------------------------------------------")
+print("Tamanho sequencia.: ",seq.get_tamanho_sequencia(orginal))
+print("Percentual de GC..: {:.2f} %".format(seq.get_percentual_GC(orginal)))
+print("==================================================================Done!")
+
+print("b)Usando k=31 obtenha as sequências kmers, salve em arquivo e Responder:")
+print("------------------------------------------------------------------------")
+"""Dividir a sequência com k = 31 usando janela desllizante. Salvar todas as sequencias resultantes no arquivo '.\saida\reads.fasta' identificando as sequencia por um número '>n' 
+Responder quantas sequências serão armazenadas nesse arquivo? Opcional: tente reconstruir o genoma original com base no arquivo “reads.fasta”.
+"""
+k = 31
+k_mers = seq.get_kmers(orginal,k)
+#Criar um diretório <saida>, somente se o mesmo não existir
+path_dir = "." + os.sep + "saida"
+if not os.path.exists(path_dir): #ou if not os.path.isdir(path_dir):
+    os.makedirs(path_dir)      
+#Gravando o arquivo Fasta
+saida = "." + os.sep + "saida" + os.sep + "reads" + ".fasta"
+with open (saida, "w") as arq:
+    for n, mer in enumerate(k_mers):
+        arq.write(">" + str(n + 1)+"\n") # Escreve a identificação da seqência
+        arq.write(mer + "\n") #Escreve a sequencia mer no arquivo
+        arq.write("\n") #adicona uma linha em branco no arquivo
+print("Sequencias armazenadas no arquivo: ", len(k_mers)) #Poderia ser feito com ->  seq.get_tamanho_sequencia(orginal) - k + 1
+print("Opcional: Reconstruindo o Genoma a partir dos k-mers do arquivo reads.fasta. Por favor aguarde...")
+
+assembly = Bioprof() #Objeto com os mers (partes do genoma)
+
+#importa todos os k-mers que forma o genoma para o novo objeto assembly
+assembly.leiaArquivoFasta(saida)
+seq.add_seq("Obtido","Genoma construido a partir dos kmers",assembly.get_assembly_kmers()) #adiciona a sequencia construida a partir do kmers
+print("Comparando:")
+print("Tamanho Genoma original: ",seq.get_tamanho_sequencia(orginal))
+print("Tamanbo Genoma obtido..: ",seq.get_tamanho_sequencia("Obtido"))
+print("Composição Genoma orginal..:",seq.get_composicao_seq(orginal))
+print("Composição Genoma obtido...:",seq.get_composicao_seq('Obtido'))
+print("Comparação identica: ", "Sucesso!" if seq.get_sequencia(orginal) == seq.get_sequencia("Obtido") else "Falha na construção do Genoma pelso k-mers!")
+#print("Sequencia Genoma:",seq.get_sequencia(orginal))
+#print("Sequencia obtida:",seq.get_sequencia('Obtido'))
+print("==================================================================Done!")
+
+print("c) CDS - ")
+print("-----------------------------------------------------")
+"""
+c) Identifique todas as regiões codificantes (CDS) deste genoma. Considere que a região
+codificante começa com uma metionina e termina com um stop códon; considere as seis
+janelas de codificação (não há problema seu programa retornar falsos-positivos). Salve
+cada possível CDS em um arquivo no formato FASTA (os arquivos devem ser salvos
+em uma pasta chamada “cds”).
+"""
+
+
+print("==================================================================Done!")
+
+
+
+
+
+
